fdbb4951505a5df710c45d9cb4f259e3d823a77b
Daniel Tenner <daniel.git@tenner.org>
2008-05-14 19:28:45 +0100

    AMF0 encoder can now encode numbers

--- lib/ruby_amf/io/amf0/encoder.rb
+++ lib/ruby_amf/io/amf0/encoder.rb
@@ -3,14 +3,27 @@ module RubyAMF
     module AMF0
       class Encoder
 
-        attr_accessor :stream
+        attr_reader :stream
         
         def initialize(stream="")
           @stream = stream
+          @writer = RubyAMF::IO::Util::BigEndianWriter.new(@stream)
+        end
+        
+        def stream=(stream)
+          @stream = stream
+          @writer.stream = @stream
         end
         
         def encode(value)
-          @stream << value
+          case value
+          when Numeric        : encode_number(value)
+          end
+        end
+        
+        def encode_number(value)
+          @stream << RubyAMF::IO::AMF0::Types::NUMBER
+          @writer.write(:double, value)
         end
         
       end
