a32782b8a6cdfc81b35134a15ded4635bd87ec42
Daniel Tenner <daniel.git@tenner.org>
2008-07-11 12:27:12 +0100

    Added AMF3 types and spec

---
+++ lib/flails/io/amf3/types.rb
@@ -0,0 +1,22 @@
+module Flails
+  module IO
+    module AMF3
+      module Types
+        UNDEFINED           = 0x00
+        NULL                = 0x01
+        BOOL_FALSE          = 0x02
+        BOOL_TRUE           = 0x03
+        INTEGER             = 0x04
+        NUMBER              = 0x05
+        STRING              = 0x06
+        XML                 = 0x07
+        DATE                = 0x08
+        ARRAY               = 0x09
+        OBJECT              = 0x0a
+        XMLSTRING           = 0x0b
+        BYTEARRAY           = 0x0c
+      end
+    end
+  end
+end
+
