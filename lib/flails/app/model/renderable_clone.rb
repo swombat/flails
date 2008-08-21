58a27974d44655956a000f3e5247070c457ab168
Daniel Tenner <daniel.git@tenner.org>
2008-08-21 14:10:39 +0100

    Tidy-up

--- lib/flails/app/model/renderable_clone.rb
+++ lib/flails/app/model/renderable_clone.rb
@@ -18,11 +18,7 @@ module Flails
         def clone_attributes
           @renderable_attributes = {}
           @original.renderable_attributes.each do |key, value|
-            RAILS_DEFAULT_LOGGER.debug "=== #{key.inspect} = > #{value.inspect} =? #{value.is_a?(Enumerable)}\n\n"
-            
             @renderable_attributes[key] = render_value(value)
-            
-            RAILS_DEFAULT_LOGGER.debug "....... set to #{@renderable_attributes[key].inspect}"
           end
         end
         
@@ -30,11 +26,7 @@ module Flails
           if value.is_a?(Hash) || value.is_a?(Array)
             value.depth(@max_depth-1)
           elsif value.respond_to?(:depth)
-            if (@max_depth < 0)
-              nil
-            else
-              value.depth(@max_depth-1)
-            end
+            @max_depth < 0 ? nil : value.depth(@max_depth-1)
           else
             value
           end
