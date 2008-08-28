4e7a3934eaaeae4601f0b574de9980f85409c20a
Daniel Tenner <daniel.git@tenner.org>
2008-08-28 13:38:38 +0100

    Returning a [nil] array to enable the Flex front-end to see the difference between an empty array and a depth cut-off

--- monkey_patches.rb
+++ monkey_patches.rb
@@ -4,7 +4,7 @@
 class Array
   def depth(max_depth)
     if (max_depth < 0)
-      []
+      [nil]
     else
       self.collect { |value| value.respond_to?(:depth) ? value.depth(max_depth-1) : value }
     end
@@ -14,7 +14,7 @@ end
 class Hash
   def depth(max_depth)
     if (max_depth < 0)
-      {}
+      nil
     else
       self.inject({}) { |memo, (key, value)| memo[key] = value.respond_to?(:depth) ? value.depth(max_depth-1) : value }
     end
