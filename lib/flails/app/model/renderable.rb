af98a45fb30992ed43ec8b0af4173f0b3d18305f
Daniel Tenner <daniel.git@tenner.org>
2008-09-29 16:07:49 +0100

    Yeah, that might help a little

--- lib/flails/app/model/renderable.rb
+++ lib/flails/app/model/renderable.rb
@@ -18,26 +18,31 @@ module Flails
           options[:indent] ||= 2
           xml = options[:builder] ||= Builder::XmlMarkup.new(:indent => options[:indent])
           xml.instruct! unless options[:skip_instruct]
-          xml.tag!(self.class.name) do |r|
+          xml.tag!(xml_key(self.class.name)) do |r|
             self.renderable_attributes.each do |key, value|
               if value.is_a?(Flails::App::Model::Renderable)
-                r.tag!(key, value.to_xml)
+                r.tag!(xml_key(key), value.to_xml)
               elsif value.is_a?(Array)
-                r.tag!(key) do
+                r.tag!(xml_key(key)) do
                   value.each do |sub_value|
                     if (sub_value.is_a?(Flails::App::Model::Renderable))
-                      r.tag(sub_value.class.to_s, sub_value.to_xml)
+                      r.tag(xml_key(sub_value.class.to_s), sub_value.to_xml)
                     else
                       r.item(sub_value.to_s)
                     end
                   end
                 end
               else
-                r.tag!(key, value.to_s)
+                r.tag!(xml_key(key), value.to_s)
               end
             end
           end
         end
+        
+        def xml_key(key)
+          key.to_s.tableize.singularize.dasherize
+        end
+        
       end
     end
   end
